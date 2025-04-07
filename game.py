f603f1c77b25e63dc6d8dccfe07787f5c06ab292
Gas-Part <dreanogaspard@gmail.com>
2025-04-07 14:12:39 +0200

    Création du prototype
    carte nul; perso qui se déplace mais sans anim

--- game.py
+++ game.py
@@ -1,26 +1,56 @@
 import pygame
 import pytmx
 import pyscroll
+import pytmx.util_pygame
 from player import Player
 
 class Game:
 
     def __init__(self):
         #fenêtre du jeu
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((1920, 1080))
         pygame.display.set_caption("Jeux 2D")
 
         #charger la carte
-
+        tmx_data = pytmx.util_pygame.load_pygame("assets\carte.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.BufferedRenderer(map_data, self.screen.get_size(), clamp_camera=True)
+        map_layer.zoom = 2
 
         #générer le joueur
-        self.player = Player()
+        player_position = tmx_data.get_object_by_name("player")
+        self.player = Player(player_position.x, player_position.y)
 
+        #desiner le groupe de calques
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
+        self.group.add(self.player)
+
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+
+
+       
     def run(self):
+
+        clock = pygame.time.Clock()
         #garder la fenêtre ouverte
         running = True
         while running:
+            self.handle_input()
+            self.group.update()
+            self.group.center(self.player.rect.center)
+            self.group.draw(self.screen)
+            pygame.display.flip()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+            clock.tick(60)
         pygame.quit()
--- player.py
+++ player.py
@@ -2,13 +2,26 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x ,y):
         super().__init__()
-        self.sprite_sheet = pygame.image.load("assets/player.png")
+        self.sprite_sheet = pygame.image.load("assets\\player_sprite_sheet\\PNG\\Unarmed_Idle\\Unarmed_Idle_full.png").convert_alpha()
+        self.sprite_sheet.set_colorkey([0, 0, 0])
         self.image = self.get_image(0, 0)
+        self.image.set_colorkey([0, 0, 0])
         self.rect = self.image.get_rect()
+        self.position = [x, y]
+
+    def move_right(self): self.position[0] += 5
+    def move_left(self): self.position[0] -= 5
+    def move_up(self): self.position[1] -= 5   
+    def move_down(self): self.position[1] += 5
+
+
+    
+    def update(self):
+        self.rect.topleft = self.position
 
     def get_image(self, x, y):
-        image = pygame.Surface([32, 32])
-        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
+        image = pygame.Surface([64, 64])
+        image.blit(self.sprite_sheet, (0, 0), (x, y, 64, 64))
         return image
