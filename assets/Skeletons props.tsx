e8a007e5bcee7e22240fde1d79e1d8670823340b
Gas-Part <dreanogaspard@gmail.com>
2025-04-28 15:19:28 +0200

    Update de la carte
    WIP

---
+++ assets/Skeletons props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Skeletons props" tilewidth="16" tileheight="16" tilecount="2112" columns="48">
+ <image source="skeleton/PNG/Objects.png" trans="000000" width="768" height="704"/>
+</tileset>
---
+++ assets/sandwater.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="sandwater" tilewidth="16" tileheight="16" tilecount="120" columns="10">
+ <image source="lpc_terrain/twosided/sandwater.png" trans="000000" width="160" height="192"/>
+</tileset>
