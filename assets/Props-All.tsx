8c5eb53cb660a12b12249e957bbba8fc90902631
phileascrei <phileascreignou22@gmail.com>
2025-05-12 15:19:24 +0200

    on recommance a zero
    on change de projet, on refait un mortal combat, l'ennemi est un ia

--- assets/Props-All.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Props-All" tilewidth="16" tileheight="16" tilecount="180" columns="15">
- <image source="Hana Caraka - Topdown Tileset [sample]/Props/Props-All.png" trans="000000" width="240" height="192"/>
-</tileset>
